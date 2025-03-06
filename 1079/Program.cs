5ae21103264010814d765abbbb787c0437ba7a11
gabriel dias <bieldias023@outlook.com>
2025-03-06 12:44:16 -0300

    COMIT

---
+++ 1079/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio1079
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int n;
+
+            Console.WriteLine("Digite o número de casos de testes que serão realizados: ");
+            n = int.Parse(Console.ReadLine());
+
+            double[] casosTeste = new double[3];
+            double mediaPonderada;
+            for (int i = 1; i < n; i++)
+            {
+                Console.WriteLine($"Digite os 3 números do caso de teste de número {i} ");
+
+                for (int j = 0; j < 3; j++)
+                {
+                    casosTeste[j] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                }
+                mediaPonderada = ((casosTeste[0] * 2) + (casosTeste[1] * 3) + (casosTeste[2] * 5)) / 10;
+                Console.WriteLine($"Média ponderada: {mediaPonderada:F1}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 1101.2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Desafio1101
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Digite dois pares de números, até um chegar no 0: ");
+                string[] pares = Console.ReadLine().Split();
+                int m = int.Parse(pares[0]);
+                int n = int.Parse(pares[1]);
+
+                if (m <= 0 || n <= 0)
+                {
+                    break;
+                }
+
+                if (m > n)
+                {
+                    int newM;
+                    newM = n;
+                    n = m;
+                    m = newM;
+                }
+
+                int soma = 0;
+
+                for (int i = m; i <= n; i++)
+                {
+                    Console.Write(i + " ");
+                    soma += i;
+                }
+
+                Console.WriteLine($"Sum={soma}");
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
